104b8a0dea0746ad4882812ecc02096cd602a7b2
sailingdev <sailingdev123@gmail.com>
2021-02-22 20:07:34 +0800

    enable ssl

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://tortu-elearning-platform.herokuapp.com/'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
