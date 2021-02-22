e35094848b6278ca4927c3121c156d5a92474dac
sailingdev <sailingdev123@gmail.com>
2021-02-22 21:18:55 +0800

    add mix to application.blade.php

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
@@ -9,25 +9,25 @@
 
     <title>Vuexy  - Vuejs, HTML & Laravel Admin Dashboard Template</title>
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/iconfont.css')}}">
-    <link rel="stylesheet" href="{{ asset('css/material-icons/material-icons.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/vuesax.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/prism-tomorrow.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/main.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/iconfont.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/material-icons/material-icons.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/vuesax.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/prism-tomorrow.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/app.css')) }}">
 
     <!-- Favicon -->
     <link rel="shortcut icon" href="{{ asset('images/logo/favicon.png') }}">
-  </head>
-  <body>
-    <noscript>
-      <strong>We're sorry but Vuexy  - Vuejs, HTML & Laravel Admin Dashboard Template doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
-    </noscript>
-    <div id="app">
-    </div>
+</head>
+<body>
+<noscript>
+    <strong>We're sorry but Vuexy  - Vuejs, HTML & Laravel Admin Dashboard Template doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
+</noscript>
+<div id="app">
+</div>
 
-    <!-- <script src="js/app.js"></script> -->
-    <script src="{{ asset('js/app.js') }}"></script>
+<!-- <script src="js/app.js"></script> -->
+<script src="{{ asset(mix('js/app.js')) }}"></script>
 
-  </body>
+</body>
 </html>
