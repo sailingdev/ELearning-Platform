a3b492983fcbb02f79dce877abd6ef5910d7962b
sailingdev <sailingdev123@gmail.com>
2021-02-26 05:57:58 +0800

    add social share button

---
+++ resources/js/frontend/src/components/TheSocialButton.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="mt-2 social-group" :style="{'background-color': isColored}">
+        <a class="cursor-on item-3" @click="btn_open" :style="{color: status ? '#fff' : '#0811fb' }">
+            <i :class="{fa:true, 'fa-share-alt':!status, 'fa-close': status}"></i>
+        </a>
+        <div class="item-1 cursor-on" :style="{display: isOpened}">
+            <i class="lni-facebook-filled"></i>
+        </div>
+        <div class="item-2 cursor-on" :style="{display: isOpened}">
+            <i class="lni-twitter-filled"></i>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'TheSocialButton',
+      data(){
+          return {
+              status: false
+          }
+      },
+      computed: {
+          isOpened(){
+              return this.status === false ? 'none' : 'block'
+          },
+          isColored(){
+              return this.status ? '#0811fb' :  '#fff'
+          }
+      },
+      methods: {
+          btn_open(){
+              this.status = !this.status
+          }
+      }
+  }
+</script>
+
+<style scoped>
+
+</style>
