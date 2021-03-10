3b4e2dd5715e58064bac8c38f69a99bee8fbbc51
sailingdev <sailingdev123@gmail.com>
2021-03-10 17:38:47 +0800

    Change TheVocabularyCarousel.vue to use Audio HTML tag and implement twice repeat.

--- resources/js/frontend/src/components/TheVocabularyCarousel.vue
+++ resources/js/frontend/src/components/TheVocabularyCarousel.vue
@@ -21,7 +21,7 @@
                     </div>
                     <div id="lesson-carousel" style="" class="slick-initialized slick-slider">
                         <button class="slick-prev slick-arrow lni-arrow-left lni-bold" aria-label="Previous"
-                                type="button" aria-disabled="true" @click="previous"
+                                type="button" aria-disabled="true" @click="directionPlay(false)"
                         >
                         </button>
                         <div class="slick-list draggable" style="padding: 0px 20px;">
@@ -53,15 +53,15 @@
                                                                  aria-hidden="true">
                                                                 © Copyright LingoHut.com 771363
                                                             </div>
-                                                            <div class="intro-s-text">
+                                                            <div class="intro-s-text" @click="directPlay(true)">
                                                                 <div class="intro-text spalan-text">
                                                                     Goeie môre
                                                                 </div>
                                                             </div>
                                                         </div>
                                                         <div class="intro-repeat-area">
-                                                            <audio v-on:ended="onEnded" @play="true" style="display: none">
-                                                                <source v-for="n in 2" :key="n" :src="item.src"
+                                                            <audio ref="audio" v-on:ended="onEnded">
+                                                                <source :src="item.src"
                                                                     type="audio/mp3">
                                                             </audio>
                                                             <div class="intro-repeat-self-text">
@@ -77,7 +77,7 @@ Repeat out loud
                                                                     padding: 12px;
                                                                     font-size: 26px;
                                                                     color: rgba(0,0,0,.54);"
-                                                                   v-on:click="audioControl"
+                                                                   v-on:click="directPlay(false)"
                                                                 >
                                                                 </i>
                                                             </div>
@@ -91,7 +91,7 @@ Repeat out loud
                             </div>
                         </div>
                         <button class="slick-next slick-arrow lni-arrow-right lni-bold" aria-label="Next" type="button"
-                                style="display: block;" aria-disabled="false" v-on:click="next">
+                                style="display: block;" aria-disabled="false" v-on:click="directionPlay(true)">
                         </button>
                     </div>
                     <div id="slide-footer">
@@ -134,31 +134,58 @@ Repeat out loud
                 ],
                 currentSlide: 0,
                 currentAudio: null,
-                isPlayable: true
+                isPlayable: true,
+                isPlayable_temp: true,
+                isDirection: false
             }
         },
         methods: {
-            previous () {
-                this.currentSlide = this.currentSlide === 0 ? this.slides.length - 1 : this.currentSlide
-                this.currentSlide -= 1
+            directionPlay(isNext){
+                this.isPlayable_temp = false
+                const val = this.currentSlide
+                let stoppedIndex = val
+                isNext ? this.next(val) : this.previous(val)
+                this.isDirection = true
+                let oldAudio = this.$refs.audio[stoppedIndex]
+                oldAudio.pause()
+                oldAudio.currentTime = 0
+                this.timeDelay()
+            },
+            directPlay(isOnce){
+                if(!isOnce)  this.isDirection = true
+                this.isPlayable_temp = !this.isPlayable_temp
                 this.audioControl()
             },
-            next () {
-                this.currentSlide = this.currentSlide >= this.slides.length - 1 ? -1 : this.currentSlide
-                this.currentSlide += 1
-                this.audioControl()
+            previous () {
+                this.currentSlide = val === 0 ? this.slides.length - 1 : val -1
+            },
+            next (val) {
+                this.currentSlide = val === this.slides.length - 1 ? 0 : val +1
+            },
+            timeDelay(){
+                setTimeout(()=> {
+                    this.isPlayable = true
+                    this.audioControl()
+                }, 700)
             },
             audioControl () {
-                this.audio()
-                let audio = this.currentAudio
-                this.isPlayable? audio.play() : audio.pause()
+                let audio = this.$refs.audio[this.currentSlide]
+                if(this.isPlayable)
+                    audio.play()
+                else {
+                    audio.pause()
+                    audio.currentTime = 0
+                }
                 this.isPlayable = !this.isPlayable
             },
             onEnded () {
-                this.isPlayable = true
-            },
-            audio () {
-                this.currentAudio = new Audio(this.slides[this.currentSlide].src)
+                if (this.isDirection) {
+                    this.timeDelay()
+                } else {
+                    this.isPlayable_temp = true
+                    this.isPlayable = true
+                }
+                this.isDirection = false
             },
         },
         computed: {
@@ -167,16 +194,9 @@ Repeat out loud
                 return `translate3d(${val}px, 0px, 0px)`
             },
             audioBtnClass () {
-                return this.isPlayable === true ? 'lni-play' : 'lni-pause'
+                return this.isPlayable_temp === true ? 'lni-play' : 'lni-pause'
             },
         },
-        watch:{
-            currentAudio(val, oldVal){
-                if (val.onended){
-                    console.log(123)
-                }
-            }
-        }
     }
 </script>
 
