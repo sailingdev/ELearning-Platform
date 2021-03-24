511e9300ffd011cedcd9e857042cc5c1c1abacd2
sailingdev <sailingdev123@gmail.com>
2021-03-24 09:53:48 +0800

    Make Sibebar for lesson list

--- resources/js/src/views/lesson/LessonList.vue
+++ resources/js/src/views/lesson/LessonList.vue
@@ -1,13 +1,85 @@
 <template>
+    <div id="chat-app" class="border border-solid d-theme-border-grey-light rounded relative overflow-hidden">
+        <vs-sidebar class="items-no-padding" parent="#chat-app" :click-not-close="clickNotClose"
+                    :hidden-background="clickNotClose" v-model="isChatSidebarActive" id="chat-list-sidebar">
+            <vs-row vs-align="flex-end" vs-type="flex" vs-justify="flex-end" class="chat__profile-search flex p-4">
+                <vs-col vs-type="flex" vs-justify="flex-end" class=" h-10" >
+                    <feather-icon class="md:justify-items-end lg:hidden -ml-3 cursor-pointer" icon="XIcon" @click="toggleChatSidebar(false)" />
+                </vs-col>
+            </vs-row>
+            <vs-divider class="d-theme-border-grey-light m-0" />
+            <component :is="scrollbarTag" class="chat-scroll-area pt-4" :settings="settings" :key="$vs.rtl">
+                <div class="chat__contacts">
+                </div>
+            </component>
+        </vs-sidebar>
 
+        <!-- RIGHT COLUMN -->
+        <div
+            class="chat__bg no-scroll-content chat-content-area border border-solid d-theme-border-grey-light border-t-0 border-r-0 border-b-0"
+            :class="{'sidebar-spacer--wide': clickNotClose}">
+            <div class="chat__navbar">
+                <lesson-navbar :isSidebarCollapsed="!clickNotClose" @openContactsSidebar ="toggleChatSidebar(true)" ></lesson-navbar>
+            </div>
+            <component :is="scrollbarTag" class="chat-content-scroll-area border border-solid d-theme-border-grey-light" :settings="settings" ref="chatLogPS" :key="$vs.rtl">
+
+            </component>
+        </div>
+
+    </div>
 </template>
 
 <script>
-  export default {
-    name: 'LessonList'
-  }
+    import LessonNavbar from './LessonNavbar'
+    import VuePerfectScrollbar from 'vue-perfect-scrollbar'
+    export default {
+        name: 'LessonList',
+        components: {
+            LessonNavbar,
+            VuePerfectScrollbar
+        },
+        data () {
+            return {
+                clickNotClose: true,
+                isChatSidebarActive  : true,
+                settings             : {
+                    maxScrollbarLength : 60,
+                    wheelSpeed         : 0.70
+                },
+            }
+        },
+        watch: {
+            windowWidth () {
+                this.setSidebarWidth()
+            }
+        },
+        computed: {
+            scrollbarTag(){
+              return this.$store.getters.scrollbarTag
+            },
+            windowWidth () {
+                return this.$store.state.windowWidth
+            },
+        },
+        created(){
+          this.setSidebarWidth()
+        },
+        methods: {
+            toggleChatSidebar (value = false) {
+                if (!value && this.clickNotClose) return
+                this.isChatSidebarActive = value
+            },
+            setSidebarWidth () {
+                if (this.windowWidth < 1200) {
+                    this.isChatSidebarActive = this.clickNotClose = false
+                } else {
+                    this.isChatSidebarActive = this.clickNotClose = true
+                }
+            },
+        }
+    }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+    @import "@sass/vuexy/apps/chat.scss";
 </style>
---
+++ resources/js/src/views/lesson/LessonNavbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="chat__header">
+        <vs-navbar collapse  class="p-4 flex navbar-custom" color="white" type="flat">
+            <div class="relative flex mr-4 h-10">
+                <feather-icon icon="MenuIcon" class="mr-4 cursor-pointer" v-if="isSidebarCollapsed" @click.stop="$emit('openContactsSidebar')" />
+
+                <div class="h-3 w-3 border-white border border-solid rounded-full absolute right-0 bottom-0 bg-success"></div>
+            </div>
+            <h6>firsthisd</h6>
+            <vs-spacer></vs-spacer>
+        </vs-navbar>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'LessonNavbar',
+      props:{
+          isSidebarCollapsed: {
+              type: Boolean,
+              required: true
+          }
+      }
+  }
+</script>
+
+<style scoped>
+
+</style>
